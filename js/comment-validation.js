c992a0bb90f14c7024fac5a9543078198add3441
Aline096 <anoclez@gmail.com>
2022-12-07 22:19:56 +0200

    Validation and Local storage

---
+++ js/comment-validation.js
@@ -0,0 +1,40 @@
+const nameRegex= /^[a-zA-Z]+ [a-zA-Z]+$/;
+const required= 10;
+
+
+document.querySelector('#comment-form').addEventListener('submit', (event) => {
+    event.preventDefault() 
+    const user1= document.getElementById('user')
+    const comment1= document.getElementById('comment')
+
+    user1.addEventListener('input', ()=>{
+        validate();
+    })
+    comment1.addEventListener('input', ()=>{
+        validate();
+    })
+    validate();
+    
+})
+
+function validate(){
+    const user= document.getElementById('user').value;
+    if (user.match(nameRegex)) {
+        document.querySelector(".name_error").textContent = ""
+        console.log(user)   
+    } else if(user.length == 0){
+        document.querySelector(".name_error").textContent = "Enter your name"
+    } else {
+        document.querySelector(".name_error").textContent = "Enter your name ";
+    }
+
+    const comment= document.getElementById('comment').value;
+    if (comment.length-required>0) {
+        document.querySelector(".comment_error").textContent = ""
+        console.log(comment)   
+    } else if(comment.length == 0){
+        document.querySelector(".comment_error").textContent = "Enter The comment, Minimum 10 characters"
+    } else {
+        document.querySelector(".comment_error").textContent = "Enter the comment, Minimum 10 characters ";
+    }
+}
--- js/sign-in-validation.js
+++ js/sign-in-validation.js
@@ -1,29 +1,61 @@
-var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-var passwordRegex= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-document.querySelector('#form_login').addEventListener('submit', (event) => {
-    event.preventDefault()
-    const email = event.target.elements['email'].value;
-    const password = event.target.elements['password'].value;
-    // if (email.includes('@')) {
-        if (email.match(emailRegex)) {
-        document.querySelector(".email_error").textContent = ""
-        console.log(email)
-        
-    } else if(email.length == 0){
-        document.querySelector(".email_error").textContent = "Enter your email"
+localStorage.setItem('admin', JSON.stringify({email: "admin@portfolio.me", password: 'Password@1234'}))
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const passwordRegex= /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+let isEmailValid = false
+let isPasswordValid = false
+
+let formError = document.querySelector(".form_error")
+
+document.querySelector('#form_login').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email= e.target.elements['email'].value;
+    const password= e.target.elements['password'].value;
+    const adminCredential = JSON.parse(localStorage.getItem('admin'))
+    if(isEmailValid && isPasswordValid){
+        if (adminCredential.email === email && adminCredential.password === password) {
+            console.log('success');
+            localStorage.setItem("isLoggedIn", JSON.stringify(true))
+            location.assign('/html/dashboard.html')
+        } else {
+
+            formError.textContent = "Invalid Credential"
+            setTimeout(() => {
+                formError.textContent = ""
+            }, 5000)
+        }
+    }
+});
+
+let emailError = document.querySelector(".email_error");
+
+document.getElementById('input_email').addEventListener('input', (e) => {
+    const email = e.target.value
+    if (e.target.value.match(emailRegex)) {
+        isEmailValid = true
+        emailError.textContent = ""
+    }else if(email.length == 0){
+        isEmailValid = false
+        emailError.textContent = "Enter your email"
     }else{
-        document.querySelector(".email_error").textContent = "Email is Invalid"
+        isEmailValid = false
+        emailError.textContent = "Email is Invalid"
     }
-
-    
-        if (password.match(passwordRegex)) {
-        document.querySelector(".password_error").textContent = ""
-        console.log(password)
         
-    } else if(password.length == 0){
-        document.querySelector(".password_error").textContent = "Enter your password, Minimum 8 characters with at least one letter,one digit and one special character "
-    } else {
-        document.querySelector(".password_error").textContent = " Password is Invalid";
-    }
+});
 
+let passwordError = document.querySelector(".password_error")
+
+document.getElementById('input_password').addEventListener('input', (e) => {
+    const password = e.target.value
+    if (password.match(passwordRegex)) {
+        isPasswordValid = true
+        passwordError.textContent = "";
+    } else if(password.length == 0){
+        isPasswordValid = false
+        passwordError.textContent = "Enter your password, Minimum 8 characters with at least one letter,one digit and one special character ";
+    } else {
+        isPasswordValid = false
+        passwordError.textContent = " Password is Invalid";
+    }
 })
+
